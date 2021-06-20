48e78960dddfad18d5e266f54f960ac6604e7959
area1110 <69160530+area1110@users.noreply.github.com>
2021-06-20 13:27:11 +0700

    fix inner text

--- JS_Week2/gallery.js
+++ JS_Week2/gallery.js
@@ -2,9 +2,12 @@ function upDate(previewPic) {
   x = document.getElementById("image");
   y = previewPic.getAttribute("src");
   x.style.backgroundImage = "url('" + y + "')";
+  x.innerHTML = previewPic.getAttribute("alt");
+  
 }
 
 function unDo() {
   x = document.getElementById("image");
-  x.style.backgroundImage = "url('')";
+  x.style.backgroundImage = "none";
+  x.innerHTML = "Hover over an image below to display here.";
 }
